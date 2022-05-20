928b032568b83e3948d20e58fb2f0fb39bb2967b
Landeiras <landeirasluis@gmail.com>
2022-05-20 10:24:43 +0100

    asd

--- .idea/src/main/java/com/example/designprogram/HelloController.java
+++ .idea/src/main/java/com/example/designprogram/HelloController.java
@@ -1,12 +1,15 @@
-package com.example.paint;
+package com.example.designprogram;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.*;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseButton;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.FileChooser;
@@ -17,9 +20,8 @@ import java.io.IOException;
 
 /*
 Objetivos:
--Salvar imagem
--Desenhar fractais
--Permitir zoom
+-Desenhar fractais(Beta)
+-Permitir zoom(Beta)
 -Criar a opção de balde de tinta
  */
 public class HelloController {
@@ -34,11 +36,9 @@ public class HelloController {
     @FXML
     private Slider tamfiguras, grofiguras;
     @FXML
-    private CheckBox triangulo, quadrado, circulo, balde;
+    private CheckBox triangulo, quadrado, circulo, bucket;
     @FXML
     private Label label, label2;
-    @FXML
-    private TextField zoom;
 
     //Método responsável pelas alterações do canvas
     @FXML
@@ -56,7 +56,7 @@ public class HelloController {
                     quadrado.setSelected(false);
                     circulo.setSelected(false);
                     triangulo.setSelected(false);
-                    balde.setSelected(false);
+                    bucket.setSelected(false);
                     pencil.setSelected(false);
                 } else if (pencil.isSelected()) {
                     coords.setFill(colorPicker.getValue());
@@ -71,13 +71,13 @@ public class HelloController {
                 double grossura = Double.parseDouble(String.valueOf(grofiguras.getValue()));
                 double x = e.getX() - size / 2;
                 double y = e.getY() - size / 2;
+                coords.setLineWidth(grossura);
                 if (circulo.isSelected()) {
                     c.setCenterX(x + size / 2);
                     c.setCenterY(y + size / 2);
                     c.setRadius(size / 2);
                     coords.setStroke(colorPicker.getValue());
                     coords.strokeOval(x, y, size, size);
-                    coords.setLineWidth(grossura);
                 }
                 if (quadrado.isSelected()) {
                     r.setX(x);
@@ -86,7 +86,6 @@ public class HelloController {
                     r.setHeight(size);
                     coords.setStroke(colorPicker.getValue());
                     coords.strokeRect(x, y, size, size);
-                    coords.setLineWidth(grossura);
                 }
                 if (triangulo.isSelected()) {
                     //triangulo retangulo
@@ -94,11 +93,10 @@ public class HelloController {
                     double[] yPoints = {y, y + size, y + size};
                     coords.setStroke(colorPicker.getValue());
                     coords.strokePolygon(xPoints, yPoints, 3);
-                    coords.setLineWidth(grossura);
                 }
-                if (balde.isSelected()) {
+                if (bucket.isSelected()) {
                     coords.setFill(colorPicker.getValue());
-                    coords.fillRect(x, y, coords.getCanvas().getHeight(), coords.getCanvas().getWidth());
+                    coords.fillRect(x, y, size, size);
                 }
             }
         });
@@ -118,14 +116,15 @@ public class HelloController {
     @FXML
     public void grossurafig(){
         grofiguras.setMax(100);
-        grofiguras.setMin(0);
+        grofiguras.setMin(1);
         double size = Double.parseDouble(String.valueOf(grofiguras.getValue()));
         int resultado2 = (int) size;
         label2.setText(String.valueOf(resultado2));
+
     }
     @FXML
     public void tamanho(){
-        pincel.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 100));
+        pincel.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 100));
     }
 
     //Método responsável por salvar o canvas
@@ -136,13 +135,16 @@ public class HelloController {
         fileChooser.setTitle("Save Image");
         fileChooser.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("PNG", "*.png"),
-                new FileChooser.ExtensionFilter("JPG", "*.jpg")
+                new FileChooser.ExtensionFilter("JPG", "*.jpg"),
+                new FileChooser.ExtensionFilter("JPEG", "*.jpeg")
         );
         File file = fileChooser.showSaveDialog(stage);
         if (file != null) {
             WritableImage writableImage = canvas.snapshot(null, null);
             try {
                 ImageIO.write(SwingFXUtils.fromFXImage(writableImage, null), "png", file);
+                ImageIO.write(SwingFXUtils.fromFXImage(writableImage, null), "jpg", file);
+                ImageIO.write(SwingFXUtils.fromFXImage(writableImage, null), "jpeg", file);
             }
             catch (IOException e) {
                 e.printStackTrace();
@@ -156,7 +158,8 @@ public class HelloController {
         FileChooser fileChooser = new FileChooser();
         fileChooser.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("PNG", "*.png"),
-                new FileChooser.ExtensionFilter("JPG", "*.jpg")
+                new FileChooser.ExtensionFilter("JPG", "*.jpg"),
+                new FileChooser.ExtensionFilter("JPEG", "*.jpeg")
         );
         File file = fileChooser.showOpenDialog(null);
         if (file != null) {
@@ -171,15 +174,53 @@ public class HelloController {
         canvas.getGraphicsContext2D().clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
     }
 
-    //Método responsável por dar zoom no canvas
+    //Método responsável por dar zoom no canvas(Beta test)
     @FXML
-    public void zoom(){
-        if (Integer.parseInt(zoom.getText()) <= 5 ){
-            int i = Integer.parseInt(zoom.getText());
-            canvas.setScaleX(i);
-            canvas.setScaleY(i);
-        }else{
-            zoom.setText("5");
+    public void zoom() {
+        canvas.setOnScroll(e -> {
+            double scaleFactor = 1.05;
+            if (e.getDeltaY() < 0) {
+                scaleFactor = 1 / scaleFactor;
+            }
+            canvas.setScaleX((canvas.getScaleX()) * scaleFactor);
+            canvas.setScaleY((canvas.getScaleY()) * scaleFactor);
+        });
+    }
+
+    @FXML
+    private void mandelbrot() {
+        GraphicsContext coords = canvas.getGraphicsContext2D();
+        double precision = Math.max((1 - (-2)) / 600, (1.2 - (-1.2)) / 400);
+        int convergenceSteps = 50;
+        for (double c = -2, xR = 0; xR < 600; c = c + precision, xR++) {
+            for (double ci = -1.2, yR = 0; yR < 400; ci = ci + precision, yR++) {
+                double convergenceValue = checkConvergence(ci, c, convergenceSteps);
+                double t1 = convergenceValue / convergenceSteps;
+                double c1 = Math.min(255 * 2 * t1, 255);
+                double c2 = Math.max(255 * (2 * t1 - 1), 0);
+
+                if (convergenceValue != convergenceSteps) {
+                    coords.setFill(Color.color(c2 / 255.0, c1 / 255.0, c2 / 255.0));
+                } else {
+                    coords.setFill(Color.PURPLE);
+                }
+                coords.fillRect(xR, yR, 1, 1);
+            }
         }
+    }
+    private int checkConvergence(double ci, double c, int convergenceSteps) {
+        double z = 0;
+        double zi = 0;
+        for (int i = 0; i < convergenceSteps; i++) {
+            double ziT = 2 * (z * zi);
+            double zT = z * z - (zi * zi);
+            z = zT + c;
+            zi = ziT + ci;
+
+            if (z * z + zi * zi >= 4.0) {
+                return i;
+            }
+        }
+        return convergenceSteps;
     }
 }
